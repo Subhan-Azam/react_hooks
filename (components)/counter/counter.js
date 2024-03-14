72401accafb23697af9ab940924df4a5fb26af0d
Subhan Azam <subhanshabeer2005@gmail.com>
2024-03-14 16:46:12 +0500

    useState

---
+++ (components)/counter/counter.js
@@ -0,0 +1,32 @@
+"use client";
+import { useState } from "react";
+
+export default function Count() {
+  const [count, setCount] = useState(0);
+  return (
+    <div className="flex gap-5">
+      <p>Count: {count}</p>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Increment
+      </button>
+      <button
+        onClick={() => {
+          (count !== 0) && setCount(count - 1);
+        }}    
+      >
+        Decrement
+      </button>
+      <button
+        onClick={() => {
+          setCount(0);
+        }}
+      >
+        Reset
+      </button>
+    </div>
+  );
+}
